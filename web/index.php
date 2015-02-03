469729ab7c3fc45135839b7e25104db69d7a1797
Misbah Khan <mk.misbah.khan@gmail.com>
2015-02-02 22:44:14 -0800

    fixed GET /images/{id}

--- web/index.php
+++ web/index.php
@@ -329,7 +329,7 @@ $app->post('/api/list', function(Request $request) use($app) {
 $app->get('/image/{id}', function($id) use($app) {
 	$st = $app['pdo']->prepare("SELECT file from images WHERE id=:id");
 	$st->execute(array(':id' => $id));
-	$row = $st->fetch_all(PDO::FETCH_ASSOC);
+	$row = $st->fetchAll(PDO::FETCH_ASSOC);
 	return $app->json($row, 200);
 }); 
 
