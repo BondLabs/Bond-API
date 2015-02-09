2907df7ffc722322410c478ab2124b462a7ca4b1
Misbah Khan <mk.misbah.khan@gmail.com>
2015-02-08 16:49:54 -0800

    temporarily change to uid

--- web/index.php
+++ web/index.php
@@ -93,7 +93,7 @@ function namesforotherusersinbonds($bid, $uid, $app) {
 	foreach($ids as $key => $name){
 		$st->execute(array(':id' => $name));				
 		$row = $st->fetch(PDO::FETCH_ASSOC);
-		$names[$key] = $row['name'];
+		$names[$name] = $row['name'];
 	}
 	return $names;
 }
