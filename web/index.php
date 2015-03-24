a551c15a11d350645bb75be7b75bfae608b7d9ad
Bryce Dougherty <bryce.dougherty@gmail.com>
2015-03-23 19:08:36 -0700

    maybe fix it? (v1)

--- web/index.php
+++ web/index.php
@@ -437,10 +437,11 @@ $app->get('/api/rawImage/{id}', function($id) use($app) {
 	$st->execute(array(':id' => $id));
 	$row = $st->fetch(PDO::FETCH_ASSOC);
 	$file = $row['file'];
+	$string = str_replace(array("\n", "\r"), '', $file);
 	//echo "<html><img src='data:image/png;base64," . $file . "'></html>";
 	//echo $file;
 	//return $file;
-	return "<html><img src=\"data:image/png;base64," . $file . "\"></html>";
+	return "<html><img src=\"data:image/png;base64," . $string . "\"></html>";
 }); 
 
 $app->post('/api/match', function(Request $request) use($app) {
